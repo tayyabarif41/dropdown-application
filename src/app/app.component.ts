9504b896282341c7e512a33973140e6d755caebc
Tayyab Arif <tayyab_arif41@yahoo.com>
2021-04-15 21:20:53 +0500

    dropdown URL in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { Component, HostListener } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
+const TREE_DATA = [
+  {
+    name: 'Random Folder',
+    type: 'Spaces',
+    children: [
+      { name: 'List 1' },
+      { name: 'List 2' },
+      { name: 'List 3' },
+    ]
+  }, {
+    name: 'Spaces',
+    type: 'Spaces',
+    children: [
+      {
+        name: 'Projects',
+        type: 'Projects',
+        children: [
+          { name: 'Broccoli' },
+          {
+            name: 'Events',
+            type: 'Events',
+            children: [
+              { name: 'Broccoli' },
+              {
+                name: 'Bookings',
+                type: 'Bookings',
+                children: [
+                  { name: 'Broccoli' },
+                  { name: 'Brussels sprouts' },
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dropdown-application';
+  addInput: string = '';
+  dropdown: boolean = false;
+  treeControl = new NestedTreeControl<any>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<any>();
+  value: any;
+  @HostListener("document:click", ["$event"]) onDocumentClick(event: any) {
+    this.dropdown = false;
+  }
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: any) => !!node.children && node.children.length > 0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTreeModule,
+    MatMenuModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
